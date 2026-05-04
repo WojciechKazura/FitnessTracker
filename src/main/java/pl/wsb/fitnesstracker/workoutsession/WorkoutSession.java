2de6b02ddf33726a6cbbe654a4a4fed16118914a
WojciechKazura <wojciech.kazura@wp.pl>
2026-05-04 22:07:35 +0200

    Dodanie 3 encji oraz testów wszystko na zielono.

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,45 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pl.wsb.fitnesstracker.training.api.Training;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 // TODO: Define the Event entity with appropriate fields and annotations
+
+@Entity
+@Table(name = "workout_session")
+@Getter
+@NoArgsConstructor
 public class WorkoutSession {
 
+
     @Id
-    private int id;
-    private int trainingId;
-    private String timestamp;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id", nullable = false)
+    private Training training;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+    @Column(nullable = false)
     private double startLatitude;
+    @Column(nullable = false)
     private double startLongitude;
+    @Column(nullable = false)
     private double endLatitude;
+    @Column(nullable = false)
     private double endLongitude;
+    @Column(nullable = false)
     private double altitude;
+
+
+
+
+
 }
